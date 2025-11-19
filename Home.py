53ccc3014a075a90fe4cae0646643ea470b552aa
Unknown <664230022@webmail.npru.ac.th>
2025-11-19 15:26:42 +0700

    new

--- Home.py
+++ Home.py
@@ -3,9 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
 
-st.header('kairung')
+st.header('Pacharaphon')
 st.image("./img/pro.jpg")
 col1, col2, col3 = st.columns(3)
