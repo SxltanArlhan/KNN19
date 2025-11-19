35ea9dc8c9e7697177f7b4edc60f15b0b4c292f1
Unknown <664230022@webmail.npru.ac.th>
2025-11-19 15:24:53 +0700

    new

---
+++ Home.py
@@ -0,0 +1,11 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.header('kairung')
+st.image("./img/pro.jpg")
+col1, col2, col3 = st.columns(3)
